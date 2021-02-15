127c600096dda65ae542b97558e5d7027c0653a1
Nepcam <cameron.nepe@gmail.com>
2021-02-15 18:21:26 +1300

    customer model & GET customer routes

--- routes/customers.js
+++ routes/customers.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../middleware/auth');
 
-// @route   POST api/customers
-// @desc    Get all customers
-// @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all customers')
+const Customer = require('../models/Customer');
+const User = require('../models/User');
+
+// @route    GET api/customers
+// @desc     Get all customers
+// @access   Private
+router.get('/', auth, async (req, res) => {
+	try {
+		const customer = await Customer.find({ user: req.user.id }).sort({
+			date: -1
+		});
+		res.json(customer);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   POST api/customers
